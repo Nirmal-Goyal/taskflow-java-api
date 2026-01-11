255d4dbbed05b33ed54dfb50fae21e5a45c39e04
Nirmal <nirmalgoyal0108@gmail.com>
2026-01-11 21:33:12 +0530

    Configure PostgreSQL and enable JPA

--- src/main/java/com/nirmal/taskflow/TaskflowJavaApiApplication.java
+++ src/main/java/com/nirmal/taskflow/TaskflowJavaApiApplication.java
@@ -2,18 +2,12 @@ package com.nirmal.taskflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(
-		exclude = {
-				DataSourceAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 public class TaskflowJavaApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TaskflowJavaApiApplication.class, args);
 	}
 }
